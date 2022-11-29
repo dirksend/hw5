7d3e4682eafa86c9b62d6f5961ef005afe8c82a2
Daniel Dirksen <dirksend@usc.edu>
2022-11-29 00:07:10 +0000

    pt2

--- schedwork.cpp
+++ schedwork.cpp
@@ -23,6 +23,49 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add your implementation of schedule() and other helper functions here
 
+// Returns true if tree and subtrees are valid.
+// Returns false if current node or subtrees are false.
+bool schedule_helper(
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    std::vector<size_t>& shiftsAssigned
+) {
+    if (dailyNeed == 0) return true;
+    if (avail.size() == 0) return false;
+    // Number of workers
+    const size_t numWorkers = avail[0].size();
+
+    // Begin next day if current day is full
+    if (sched.size() == 0U || sched.back().size() == dailyNeed) {
+        if (sched.size() == avail.size()) return true;
+        sched.push_back({});
+    }
+    
+    const size_t dayNum = sched.size() - 1;
+    vector<Worker_T>& day = sched[sched.size() - 1];
+
+    Worker_T startAt = 0;
+    if (day.size() != 0) startAt = day.back() + 1;
+
+    // Evaluate potential workers for this slot
+    day.push_back(0);
+    for (Worker_T i = startAt; i < numWorkers; i ++) {
+        if (avail[dayNum][i] && shiftsAssigned[i] < maxShifts) {
+            sched.back().back() = i;
+            shiftsAssigned[i] ++;
+            if (schedule_helper(avail, dailyNeed, maxShifts, sched, shiftsAssigned)) {
+                return true;
+            }
+            shiftsAssigned[i] --;
+        }
+    }
+    sched.back().pop_back();
+    if (sched.back().size() == 0) sched.pop_back();
+    return false;
+}
+
 bool schedule(
     const AvailabilityMatrix& avail,
     const size_t dailyNeed,
@@ -30,14 +73,11 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0U || avail[0].size() == 0U){
         return false;
     }
     sched.clear();
-    // Add your code below
-
-
-
-
+    std::vector<size_t> shiftsAssigned(avail[0].size(), 0);
+    return schedule_helper(avail, dailyNeed, maxShifts, sched, shiftsAssigned);
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -4,6 +4,7 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
@@ -11,15 +12,76 @@ using namespace std;
 
 // Add prototypes of helper functions here
 
+bool isEnglish(const std::string& word, const std::set<std::string>& dict);
+
+char* first_blank(std::string& str);
+
+void wordle_helper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+		std::set<std::string>& out
+);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
     const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
-
+    const std::set<std::string>& dict
+) {
+		std::set<std::string> out;
+		if (in.length() == 0) return out;
+		int num_blanks = 0;
+		for (const char& ch : in) {
+			if (ch == '-') num_blanks ++;
+		}
+		if (num_blanks < floating.length()) return out;
+		wordle_helper(in, floating, dict, out);
+    return out;
 }
 
 // Define any helper functions here
+
+bool isEnglish(const std::string& word, const std::set<std::string>& dict) {
+    return (dict.find(word) != dict.end());
+}
+
+char* first_blank(std::string& str) {
+	const size_t index = str.find('-');
+	if (index == std::string::npos) return nullptr;
+	return &(str.at(index));
+}
+
+void wordle_helper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+		std::set<std::string>& out
+) {
+	std::string new_in = in;
+
+	if (floating.length() > 0) {
+		// cout << "new_in: " << new_in << ", floating: " << floating << '\n';
+		for (size_t i = 0; i < new_in.length(); i ++) {
+			char& ch = new_in[i];
+			if (ch == '-') {
+				ch = floating.back();
+				wordle_helper(new_in, floating.substr(0, floating.length() - 1), dict, out);
+				ch = '-';
+			}
+		}
+	} else {
+		char* blank = first_blank(new_in);
+		if (blank != nullptr) {
+			for (char i = 'a'; i <= 'z'; i ++) {
+				*blank = i;
+				wordle_helper(new_in, floating, dict, out);
+			}
+			*blank = '-';
+		} else {
+			if (isEnglish(new_in, dict)) {
+				out.insert(new_in);
+			}
+		}
+	}
+}
